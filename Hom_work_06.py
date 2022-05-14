1de190202a8cfea285bf566e596241b301cfe3f1
Tryd0g0lik <work80@mail.ru>
2022-05-14 15:28:34 +0700

    error search

--- Hom_work_06.py
+++ Hom_work_06.py
@@ -24,14 +24,13 @@ class Mentor:
     self.courses_attached = []
 
 class Lecturer(Mentor):
-  def __init__(self):
-    pass
+  pass
+
 
 class Reviewer(Mentor, Lecturer):
-  def __init__(self):
-    pass
 
-  def rate_hw(self, student, course, grade):
+
+  def rate_hw(self, Student, course, grade):
     if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
       if course in student.grades:
         student.grades[course] += [grade]
